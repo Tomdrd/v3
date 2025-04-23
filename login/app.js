a5a14c4097b502f0846b37fa64d0a6c70f42a53c
Tomdrd <128322163+Tomdrd@users.noreply.github.com>
2025-04-23 13:26:49 -0300

    login

---
+++ login/app.js
@@ -0,0 +1,30 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDoDuXxhXa58BSfBtJUUItbk7s9YmlBOGo",
+    authDomain: "meuprojetosaude-54d64.firebaseapp.com",
+    projectId: "meuprojetosaude-54d64",
+    storageBucket: "meuprojetosaude-54d64.firebasestorage.app",
+    messagingSenderId: "105539186714",
+    appId: "1:105539186714:web:c5286a9a3f5b4bc8c161a3",
+    measurementId: "G-HF1H65STC1"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  
+  window.loginComGoogle = function () {
+    auth.signInWithPopup(provider)
+      .then((result) => {
+        const user = result.user;
+        document.getElementById('infoUsuario').innerHTML = `
+          <p><strong>Bem-vindo:</strong> ${user.displayName}</p>
+          <p><strong>Email:</strong> ${user.email}</p>
+          <img src="${user.photoURL}" width="80">
+        `;
+      })
+      .catch((error) => {
+        alert("Erro no login: " + error.message);
+      });
+  }
+  
